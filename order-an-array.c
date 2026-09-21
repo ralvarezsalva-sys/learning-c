751b5a241b54054d8d87716f04c0c1a43132101a
ralvarezsalva-sys <ralvarezsalva@gmail.com>
2026-09-21 12:42:16 +0200

    Add array sorting program in order-an-array.c
    Implement a program to input and sort an array of integers.

---
+++ order-an-array.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_NUMS 50
+
+    
+int main() {
+    int thelist[MAX_NUMS];
+    int actual_num;
+    int total_nums = 0; // Guardarà quants números ha posat l'usuari
+    
+    printf("You are going to enter all the numbers you want (max 50), once you send me something it isn't a number, that will mean you want to close.\n");
+
+    // Bucle per demanar els números
+    for (int i = 0; i < MAX_NUMS; i++) {
+        printf("Diguem el num numero %d: ", i + 1);
+        if (scanf("%d", &actual_num) == 1) {
+            thelist[i] = actual_num;
+            total_nums++; // Incrementem el comptador de números vàlids
+            while (getchar() != '\n');
+        } else {
+            // L'usuari ha escrit una lletra o caràcter no vàlid
+            while (getchar() != '\n');
+            break; // Sortim del bucle de lectura
+        }
+    }
+
+    // Ordenem el Array [86, 90, 34, 12, 7, 34, 76]
+    for (int i = 0; i < total_nums; i++) {
+        for (int j = i + 1; j < total_nums; j++) {
+            if (thelist[i] > thelist[j]) {
+                int temp = thelist[i];
+                thelist[i] = thelist[j];
+                thelist[j] = temp;
+            }
+        }
+    }
+    
+    for (int i = 0; i < total_nums; i++) {
+        printf("- %d \n", thelist[i]);
+    }
+    return 0;
+}
